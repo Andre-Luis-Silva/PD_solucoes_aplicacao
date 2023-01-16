ea14affab00ff142b1b60bdaff1ef80ba892fae2
Andre-Luis-Silva <andresilva@gea.inatel.br>
2023-01-16 17:56:24 -0300

    Iniciando o repositório

---
+++ Aplicacao_PDsolucoes/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aplicacao_PDsolucoes.c
+ *
+ * Created: 16/01/2023 17:47:18
+ * Author : André Luis da Silva
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
